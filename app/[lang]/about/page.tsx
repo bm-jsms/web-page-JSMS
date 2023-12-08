5b1a8be179e66fd6e7f46d234e99aa368e8fcafa
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-12-07 21:49:23 -0500

    remove box

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -15,11 +15,6 @@ export default async function About({
 				<h1 className='text-3xl font-bold'>{page.about.title}</h1>
 				<p className='text-gray-500'>{page.about.description}</p>
 			</div>
-			<MotionAbout>
-				<div>
-					<div className='w-10 h-10 bg-black'></div>
-				</div>
-			</MotionAbout>
 		</section>
 	);
 }
